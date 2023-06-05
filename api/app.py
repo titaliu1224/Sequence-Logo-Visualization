1403560951e673e31b39b635f7bff87ae4c519ea
tita <tiffanyliu1224@gmail.com.tw>
2023-06-05 15:40:02 +0800

    put logo.py into app.py

--- api/app.py
+++ api/app.py
@@ -53,5 +53,33 @@ def getInputData():
     global sequences, image_format
     return sequences, image_format
 
+import logomaker
+import matplotlib.pyplot as plt
+import os, matplotlib
+import numpy as np
+# from Bio import SeqIO
+
+def visualize_sequence_logo(sequences, save_path=None, image_format=None, show_img=False):
+    # 產生 logo
+    matrix = logomaker.alignment_to_matrix(sequences, to_type='counts')
+    row_sums = np.sum(matrix, axis=1)
+    frequency_matrix = matrix / np.array(row_sums)[:, np.newaxis].astype(float)
+    logo = logomaker.Logo(frequency_matrix, color_scheme='skylign_protein')
+    logo.style_spines(visible=False)
+    logo.style_spines(spines=['left', 'bottom'], visible=True)
+    logo.style_xticks(rotation=0, fmt='%d', anchor=0)
+    plt.ylabel("Frequency")
+    #logo.ax.set_ylim(0, len(sequences))
+
+    # 直接顯示圖片，debug用
+    if show_img: 
+        plt.show()
+        return
+    
+    # 儲存 logo
+    plt_path = os.path.join(save_path, 'logo.' + image_format)
+    plt.savefig(plt_path)
+    print("saved image: ", plt_path)
+
 if __name__ == '__main__':
     app.run()
